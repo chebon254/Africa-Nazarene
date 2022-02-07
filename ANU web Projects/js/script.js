dddb12fc91370e697e3dfb3b90f9a94d1736d97a
chebon254 <kelvinchebon90@gmail.com>
2022-02-07 14:03:05 +0300

    Africa Nazarene Web development project

---
+++ ANU web Projects/js/script.js
@@ -0,0 +1,34 @@
+// Button When Clicked leads to another page
+function myWorks() {
+    location.replace("works.html")
+}
+
+// Button When Clicked leads to another page
+function myContact() {
+   location.replace("contact.html")
+}
+
+// Button When Clicked leads to another page
+function myVideo() {
+   location.replace("videos.html")
+}
+
+// Button When Clicked leads to another page
+function myTestimonial() {
+   location.replace("services.html")
+}
+
+
+// Button When Clicked leads to another page
+function mySites() {
+   location.replace("sites.html")
+}
+
+// Button When Clicked leads to another page
+function myPhotos() {
+   location.replace("photos.html")
+}
+
+
+
+
